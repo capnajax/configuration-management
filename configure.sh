5c6a32041bbb3cae8181af3c0ee69c93d24ebb6a
Thomas Wilkinson <thomas.wilkinson@us.ibm.com>
2018-09-30 15:39:57 -0500

    removed unnecessary echo statement

--- configure.sh
+++ configure.sh
@@ -6,8 +6,6 @@ source $(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)/common/global.sh
 #	Run this as "./configure.sh [<config_type> [<params]] "
 #
 
-echo running config
-
 # Run the whole config
 source ${COMMON_DIR}/tasks.sh $@
 
